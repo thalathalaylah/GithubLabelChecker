d6b60aabc40f116be673b7012c0c740a5fe86e18
itiya <fragment.vertex@gmail.com>
2018-12-02 19:59:08 +0900

    [fix] 指定リポジトリでのみ判定する

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,24 @@
-let label = document.querySelector('[aria-label="Labels"]');
-if (label != null && label.innerText != null && label.innerText === 'None yet\n') {
-  alert('Labels is empty!!!!!');
+function isTarget(url, targets) {
+  let splittedUrl = document.URL.split('/');
+  if (splittedUrl.length >= 7) {
+    let host = splittedUrl[2];
+    let user = splittedUrl[3];
+    let repository = splittedUrl[4];
+    let match = function(target) {
+      return target.host === host && target.user === user && target.repository === repository;
+    };
+
+    if (targets.find(match)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+targets = [{host: 'github.com', user: 'example-user', repository: 'example-repository'}];
+if (isTarget(document.URL, targets)) {
+  let label = document.querySelector('[aria-label="Labels"]');
+  if (label != null && label.innerText != null && label.innerText === 'None yet\n') {
+    alert('Labels is empty!!!!!');
+  }
 }
