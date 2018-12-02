263c10d6a95f32b6554978c41945dde369254bc1
itiya <fragment.vertex@gmail.com>
2018-12-02 16:58:53 +0900

    [add] プルリクページでラベルが無い時にalertを出せる

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,4 @@
-console.log('test');
+let label = document.querySelector('[aria-label="Labels"]');
+if (label != null && label.innerText != null && label.innerText === 'None yet\n') {
+  alert('Labels is empty!!!!!');
+}
